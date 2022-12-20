26305416628c1c0b3a528fc529c1668b69e167f5
Benjamin Godwin <godsongodwinsaben1998@gmail.com>
2022-12-20 04:20:56 +0100

    hero+movie-card

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 
 import Navbar from './component/Navbar'
+import Hero from './component/Hero'
+
 
 function App() {
   return (
     <ChakraProvider>
       <Navbar />
+      <Hero/>
      
     </ChakraProvider>
   )
