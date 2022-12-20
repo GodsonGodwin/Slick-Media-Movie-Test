e365f02c4a92ba0668949ff454b6233a8d8e0021
Benjamin Godwin <godsongodwinsaben1998@gmail.com>
2022-12-20 03:22:24 +0100

    commit new changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import Hero from "./Components/Hero";
-import MovieContainer from "./Components/MovieContainer";
 
-import Navbar from "./Components/Navbar";
 
 function App() {
   return (
